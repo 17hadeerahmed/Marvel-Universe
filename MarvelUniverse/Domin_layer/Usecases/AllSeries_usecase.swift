3da4c3fbf7d0074a569dd30f4e49b21ffda9c930
hadeer ahmed <hader@Haders-MacBook-Pro.local>
2023-06-19 18:34:05 +0200

    add all Series Use case

--- MarvelUniverse/Domin_layer/Usecases/AllSeries_usecase.swift
+++ MarvelUniverse/Domin_layer/Usecases/AllSeries_usecase.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol AllSeriesUseCaseProtocol {
+    static func getAllSeries (complitionHandler : @escaping (SeriesRequest?) -> Void)
+
+}
+
+class  AllSeriesUseCase : AllSeriesUseCaseProtocol {
+    static func getAllSeries(complitionHandler: @escaping (SeriesRequest?) -> Void) {
+        Repositary.getAllSeries { (data: SeriesRequest?) in
+            complitionHandler(data)
+        }
+    }
+    
+    
+    
+}
